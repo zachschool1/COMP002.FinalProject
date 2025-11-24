98bd9fcb9e9748f0045b9f98ee6b103574643dba
Zachary Jordan <zachary.jordan@my.whccd.edu>
2025-11-24 11:49:41 -0800

    fixed errors involving dragging and clicking outside of the calculator putting the display as every button in the calculator

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let display = document.getElementById("display");
+let calculator = document.getElementById("calculator");
 //basically have the first value become concatenated as a string and then when i press the +-/* buttons it will change from value1 to value2 and do it based on whatever was pressed
 let value1 ="";
 let value2 ="";
@@ -12,21 +13,17 @@ function buttonClick(event) {
     let clickedButton = event.target;
     let buttonValue = clickedButton.textContent;
 //ok i have the text value of the specific thing i clicked, so from here I can start using them to concatenate to value1 or 2, i think i would need to do logic to where if the first clicked is 0 or like an operator then it wont keep going on.
-    if (buttonValue === "0" || buttonValue === NaN) {
+
+    if (buttonValue !== "0" || buttonValue !== NaN) {
         value1 += buttonValue;
-    } 
-
-}
-
-
-function calculate(event) {
-    let equals = document.getElementById("button-equals");
-    if (event.target === equals) {
-        //get value 1 value as a number and value 2 as a number and do whichever operator was clicked.
+        display.textContent = value1;
     }
+
+
 }
 
 
-document.addEventListener("click", buttonClick);
+
+calculator.addEventListener("mousedown", buttonClick);
 //im not sure how I would structure this to be like ok if the person clicks the operator symbols, then start concatenating to value 2.
 
